5fd7f7b1ef1cc08e5ddf96c875043ddd4767a342
dioh0922 <diohound@gmail.com>
2023-11-15 20:36:25 +0900

    共通ヘッダテンプレート追加

---
+++ resources/views/header.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{$title}}</title>
+    </head>
+    <div @style([
+        'padding: 10px',
+        'text-align: center',
+        'background: #1abc9c',
+        'color: white',
+        'font-size: 30px',
+        'margin-bottom: 10px'
+    ])>{{$title}}</div>
+</div>
