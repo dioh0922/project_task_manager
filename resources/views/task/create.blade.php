ad34223b4939f5b7de028d4ee08bcce39342a92f
dioh0922 <diohound@gmail.com>
2023-11-21 15:02:34 +0900

    直前の入力を再現するように追加

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -11,11 +11,11 @@
     ])>
         {{Form::open(['method' => 'POST', 'url' => 'task/'])}}
         {{Form::label('summary', 'タイトル')}}
-        {{Form::text('summary', null, ['placeholder' => 'テーマの概要を入力'])}}
+        {{Form::text('summary', old('summary'), ['placeholder' => 'テーマの概要を入力'])}}
         {{Form::label('detail', '概要')}}
-        {{Form::textarea('detail', null, ['placeholder' => '詳細を入力'])}}
+        {{Form::textarea('detail', old('detail'), ['placeholder' => '詳細を入力'])}}
         {{Form::label('reference', '参考')}}
-        {{Form::textarea('reference', null, ['placeholder' => '参考文献を入力'])}}
+        {{Form::textarea('reference', old('reference'), ['placeholder' => '参考文献を入力'])}}
         {{Form::submit('作成', [])}}
         {{Form::close()}}
     </div>
