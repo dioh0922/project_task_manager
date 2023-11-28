a9b560acbda4c03c835c1c85460151d96dfa55c3
dioh0922 <diohound@gmail.com>
2023-11-28 12:59:10 +0900

    ログイン画面のスタイル調整

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,28 @@
 <div>
     <header>
         @include('header')
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </header>
     <h1>{{$check}}</h1>
     @guest
+    <div class='container d-flex align-items-center justify-content-center'>
         {{Form::open(['method' => 'POST', 'url' => '/login'])}}
         {{Form::token()}}
-        {{Form::label('userID', 'ユーザーID')}}
-        {{Form::text('userID', null)}}
-        {{Form::label('', '')}}
-        {{Form::password('password', null)}}
-        {{Form::submit('ログイン')}}
+        <div class='row'>
+            {{Form::label('userID', 'ユーザーID')}}
+            {{Form::text('userID', null, ['class' => 'form-control mb-4'])}}
+        </div>
+        <div class='row'>
+            {{Form::label('password', 'パスワード')}}
+            {{Form::password('password', ['class' => 'form-control mb-4'])}}
+        </div>
+        <div class='row'>
+            {{Form::submit('ログイン', ['class' => 'btn btn-success btn-block md-4'])}}
+        </div>
+
         {{Form::close()}}
+    </div>
     @endguest
 
     @auth
