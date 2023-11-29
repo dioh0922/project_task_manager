a20a69379cac75ee7c2dc6d4282a9213684cfb2e
dioh0922 <diohound@gmail.com>
2023-11-29 14:34:20 +0900

    作成画面のデザイン調整

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -6,18 +6,31 @@
     <div>
         @include('error')
     </div>
-    <div @style([
-        'margin: auto',
+    <div @class([
+        'container',
+        'd-flex',
+        'align-items-center',
+        'justify-content-center'
     ])>
-        {{Form::open(['method' => 'POST', 'url' => 'task/'])}}
-        {{Form::token()}}
-        {{Form::label('summary', 'タイトル')}}
-        {{Form::text('summary', old('summary'), ['placeholder' => 'テーマの概要を入力'])}}
-        {{Form::label('detail', '概要')}}
-        {{Form::textarea('detail', old('detail'), ['placeholder' => '詳細を入力'])}}
-        {{Form::label('reference', '参考')}}
-        {{Form::textarea('reference', old('reference'), ['placeholder' => '参考文献を入力'])}}
-        {{Form::submit('作成', [])}}
-        {{Form::close()}}
+        <div>
+            {{Form::open(['method' => 'POST', 'url' => 'task/'])}}
+            {{Form::token()}}
+            <div class='row'>
+                {{Form::label('summary', 'タイトル')}}
+                {{Form::text('summary', old('summary'), ['placeholder' => 'テーマの概要を入力'])}}
+            </div>
+            <div class='row'>
+                {{Form::label('detail', '概要')}}
+                {{Form::textarea('detail', old('detail'), ['placeholder' => '詳細を入力'])}}
+            </div>
+            <div class='row'>
+                {{Form::label('reference', '参考')}}
+                {{Form::text('reference', old('reference'), ['placeholder' => '参考文献を入力', 'class' => 'mb-4'])}}
+            </div>
+            <div class='row'>
+                {{Form::submit('作成', ['class' => 'btn btn-primary'])}}
+            </div>
+            {{Form::close()}}
+        </div>
     </div>
 </div>
