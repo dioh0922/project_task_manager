337283e7ff31e06eae6445d407cb0c0e0d74dabb
dioh0922 <diohound@gmail.com>
2023-12-03 13:06:39 +0900

    編集画面レイアウト調整

--- resources/views/task/detail.blade.php
+++ resources/views/task/detail.blade.php
@@ -1,47 +1,68 @@
 <div>
     <header>
         @include('header')
-        @include('back_to_list')
     </header>
-    <div @style([
-        'margin: auto',
+
+    <div @class([
+        'container',
+        'align-items-center',
+        'justify-content-center'
     ])>
-        <p>{{$task['summary']}}</p>
-        @if($task["is_delete"] === 0)
-            {{Form::open(['method' => 'PUT', 'url' => 'task/'.$task['id']])}}
-            {{Form::token()}}
-            {{Form::label('detail', '概要')}}
-            {{Form::textarea('detail', $task['detail'], ['placeholder' => '詳細を入力'])}}
-            {{Form::label('reference', '参考')}}
-
-            <span @style([
-                'overflow-wrap: break-word'    
-            ])>{{$task['reference']}}</span>
-            {{Form::text('reference', null, ['placeholder' => '参考文献を入力'])}}
-
-            {{$task['comment']}}
-
-            {{Form::text('comment', null, ['placeholder' => '検討内容を記入'])}}
-
-            {{Form::submit('編集', ['class' => ''])}}
-            {{Form::close()}}
-        @else
-            <ul>
-                <li>{{$task['summary']}}</li>
-                <li>完了日：{{$task['closed_at']}}</li>
-                <li>概要：{{$task['detail']}}</li>
-                <li>参考</li>
-                <li>{{$task['reference']}}</li>
-            </ul>
-        @endif
         <div>
+            <div class='row'>    
+                <p>{{$task['summary']}}</p>
+            </div>
             @if($task["is_delete"] === 0)
-                {{Form::open( ['method' => 'DELETE', 'url' => 'task/'.$task['id']]) }}
-                {{Form::submit('完了', ['class' => 'btn btn-danger'])}}
+                {{Form::open(['method' => 'PUT', 'url' => 'task/'.$task['id']])}}
+                {{Form::token()}}
+                <div class='row'>
+                    <div class='col-6'>
+                        {{Form::label('detail', '概要')}}
+                        {{Form::textarea('detail', $task['detail'], ['placeholder' => '詳細を入力', 'style' => 'width:100%'])}}
+                    </div>
+                    <div class='col-6'>
+                        <div class=''>
+                            {{Form::label('reference', '参考')}}
+                            <ul>
+                                @foreach($reference as $source)
+                                    <li>{{$source['source']}}：{{$source['updated_at']}}</li>
+                                @endforeach
+                            <li>{{Form::text('reference', null, ['placeholder' => '参考文献を入力'])}}</li>
+                            </ul>
+                        </div>
+                        <div>
+                            {{Form::label('comment', '検討メモ')}}
+                            <ul>
+                                @foreach($comment as $content)
+                                    <li>{{$content['comment']}}：{{$content['updated_at']}}</li>
+                                @endforeach
+                            <li>{{Form::text('comment', null, ['placeholder' => '検討内容を記入'])}}</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                <div class='row'>
+                    {{Form::submit('編集', ['class' => 'btn btn-primary mt-4'])}}
+                </div>
                 {{Form::close()}}
             @else
-                完了済
+                <ul>
+                    <li>{{$task['summary']}}</li>
+                    <li>完了日：{{$task['closed_at']}}</li>
+                    <li>概要：{{$task['detail']}}</li>
+                    <li>参考</li>
+                    <li>{{$task['reference']}}</li>
+                </ul>
             @endif
+
+        @if($task["is_delete"] === 0)
+            {{Form::open( ['method' => 'DELETE', 'url' => 'task/'.$task['id'], 'class' => 'row']) }}
+            {{Form::submit('完了', ['class' => 'btn btn-secondary mt-4'])}}
+            {{Form::close()}}
+        @else
+            完了済
+        @endif
+
         </div>
     </div>
 </div>
