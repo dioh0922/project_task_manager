a2f53c4d681e75c87350bc3419af7cf911c80ffd
dioh0922 <diohound@gmail.com>
2023-12-04 20:22:39 +0900

    編集画面レイアウト修正

--- resources/views/task/detail.blade.php
+++ resources/views/task/detail.blade.php
@@ -3,7 +3,7 @@
         @include('header')
     </header>
 
-    <div @class([
+    <div @style(['height: 80%']) @class([
         'container',
         'align-items-center',
         'justify-content-center'
@@ -15,33 +15,42 @@
             @if($task["is_delete"] === 0)
                 {{Form::open(['method' => 'PUT', 'url' => 'task/'.$task['id']])}}
                 {{Form::token()}}
-                <div class='row'>
-                    <div class='col-6'>
+                <div class='row h-50'>
+                    <div class='col-6 border'>
                         {{Form::label('detail', '概要')}}
                         {{Form::textarea('detail', $task['detail'], ['placeholder' => '詳細を入力', 'style' => 'width:100%'])}}
                     </div>
-                    <div class='col-6'>
-                        <div class=''>
+                    <div class='col-6 h-50'>
+                        <div class='my-1'>
                             {{Form::label('reference', '参考')}}
+                        </div>
+                        <div class='overflow-auto h-75 border'>
                             <ul>
                                 @foreach($reference as $source)
                                     <li>{{$source['source']}}：{{$source['updated_at']}}</li>
                                 @endforeach
-                            <li>{{Form::text('reference', null, ['placeholder' => '参考文献を入力'])}}</li>
                             </ul>
                         </div>
-                        <div>
+                        <div class='my-1'>
+                            {{Form::text('reference', null, ['placeholder' => '参考文献を入力'])}}
+                        </div>
+
+                        <div class='my-1'>
                             {{Form::label('comment', '検討メモ')}}
+                        </div>
+                        <div class='overflow-auto h-75 border'>
                             <ul>
                                 @foreach($comment as $memo)
                                     <li>{{$memo['comment']}}：{{$memo['updated_at']}}</li>
                                 @endforeach
-                            <li>{{Form::text('comment', null, ['placeholder' => '検討内容を記入'])}}</li>
                             </ul>
+                        </div>
+                        <div class='my-1'>
+                            {{Form::text('comment', null, ['placeholder' => '検討内容を記入'])}}
                         </div>
                     </div>
 
-                <div class='row'>
+                <div class='row mt-2'>
                     {{Form::submit('編集', ['class' => 'btn btn-primary mt-4'])}}
                 </div>
                 {{Form::close()}}
