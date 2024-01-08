e323cdde0e0281bf985c86f6e733c60b3298b94b
dioh0922 <diohound@gmail.com>
2024-01-08 09:25:04 +0900

    エラー表示追加

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,7 +1,7 @@
 <div>
     <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
     @if($errors->any())
-    <div class="danger">
+    <div class="alert alert-danger">
         <ul>
             @foreach($errors->all() as $error)
                 <li>{{$error}}</li>
