ee9969ae1bde2dd5f3f614913d938035ab525496
dioh0922 <diohound@gmail.com>
2024-01-11 17:23:28 +0900

    モデルにリレーションの追加

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Comment;
+use App\Models\Reference;
 
 class Task extends Model
 {
     use HasFactory;
     protected $guarded = [''];
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public function references(){
+        return $this->hasMany(Reference::class);
+    }
 }
