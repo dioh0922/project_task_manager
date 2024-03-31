580a6edb2e5f303af5dbdbe49b6663a1b840d241
dioh0922 <diohound@gmail.com>
2024-03-31 10:35:27 +0900

    テスト追加

---
+++ tests/Feature/AnalysisTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AnalysisTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_show_analyze(): void
+    {
+        $response = $this->get('/analyze');
+        $response->assertStatus(200);
+        $response->assertSee('タスク分析');
+        //デフォルトではチェックを非活性なことは確認
+        $response->assertDontSee('checked');
+        $response->assertSee('disabled');
+
+    }
+
+    public function test_show_relation_target_checked(): void
+    {
+
+        $data = [
+            'id' => 3,
+            'close' => 1
+        ];
+        $response = $this->postJson('/analyze', $data);
+
+        $response->assertStatus(200);
+        $response->assertSee('タスク分析');
+        $response->assertSee('checked');
+    }
+
+    public function test_show_relation_default_unchecked(): void
+    {
+
+        $data = [
+            'id' => 3,
+            'close' => 0
+        ];
+        $response = $this->postJson('/analyze', $data);
+
+        $response->assertStatus(200);
+        $response->assertSee('タスク分析');
+        $response->assertDontSee('checked');
+    }
+
+
+}
